afdad8c3fcd327a2d6fccb9e50bf3a51b38c74b6
kevinrivera20 <webprogramacion673@gmail.com>
2022-05-05 18:56:58 -0500

    npm

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+import MediaPlayer from "./MediaPlayer.js"
+import AutoPlay from './plugins/AutoPlay.js' 
+
+
+const video = document.querySelector("video")
+const button = document.querySelector("button")
+const button1 = document.querySelector("#Button1")
+const player = new MediaPlayer({
+    el: video,
+    plugins: [
+    //new AutoPlay() 
+    ],
+});
+
+button.onclick = () => player.handlePlay()
+button1.onclick = () => player.toggleMute()
